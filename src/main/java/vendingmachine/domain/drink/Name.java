7adb34d597ccadcca63c2d331e31d9949321359d
gyungchan Jo <namejgc@naver.com>
2023-12-13 14:18:24 +0900

    feat(Name): 음료 상품명을 검증하는 로직 구현

---
+++ src/main/java/vendingmachine/domain/drink/Name.java
@@ -0,0 +1,37 @@
+package vendingmachine.domain.drink;
+
+import java.util.Objects;
+
+public class Name {
+    private static final int MINIMUM_LENGTH_OF_NAME = 1;
+    private static final int MAXIMUM_LENGTH_OF_NAME = 10;
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        int length = name.length();
+
+        if (length < MINIMUM_LENGTH_OF_NAME || length > MAXIMUM_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException(
+                    String.format("상품명이 공백이거나 %d자를 초과할 수 없습니다.", MAXIMUM_LENGTH_OF_NAME));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/test/java/vendingmachine/domain/drink/NameTest.java
@@ -0,0 +1,28 @@
+package vendingmachine.domain.drink;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class NameTest {
+
+    @Test
+    @DisplayName("음료 상품명에 대한 정보를 알 수 있다.")
+    void createName() {
+        Name name = new Name("abcdefghij");
+        assertThat(name).isEqualTo(new Name("abcdefghij"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "abcdefghijk"})
+    @DisplayName("상품명이 공백이거나 10자를 초과하면 예외가 발생한다.")
+    void invalidName(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("상품명이 공백이거나 10자를 초과할 수 없습니다.");
+    }
+}
