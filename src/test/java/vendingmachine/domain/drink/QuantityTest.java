ca96c5f3116b90f183718ae1b052e90e0c97f10b
gyungchan Jo <namejgc@naver.com>
2023-12-13 18:22:00 +0900

    test(QuantityTest): 수량이 0인지 판별하는 기능 테스트

--- src/test/java/vendingmachine/domain/drink/QuantityTest.java
+++ src/test/java/vendingmachine/domain/drink/QuantityTest.java
@@ -2,6 +2,8 @@ package vendingmachine.domain.drink;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -21,5 +23,15 @@ class QuantityTest {
         assertThatThrownBy(() -> new Quantity(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("상품 수량이 0보다 작을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, false", "0, true"})
+    @DisplayName("수량이 0인지 알 수 있다.")
+    void isZero(int number, boolean expected) {
+        Quantity quantity = new Quantity(number);
+        boolean actual = quantity.isZero();
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
