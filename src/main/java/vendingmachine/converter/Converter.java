e0402df9cc17a7188ef1c5436ac903327bb9226b
gyungchan Jo <namejgc@naver.com>
2023-12-13 18:09:56 +0900

    refactor(Converter): 하드 코딩된 값들 상수로 분리

--- src/main/java/vendingmachine/converter/Converter.java
+++ src/main/java/vendingmachine/converter/Converter.java
@@ -7,23 +7,30 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Converter {
+    private static final String DRINKS_DELIMITER = ";";
+    private static final String DRINK_DELIMITER = ",";
+    private static final int BEGIN_INDEX = 1;
+    private static final int TO_MAKE_END_INDEX_VALUE = 1;
+    private static final int NAME_INDEX = 0;
+    private static final int PRICE_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
 
     public static Drinks toDrinks(String readDrinks) {
-        return Arrays.stream(readDrinks.split(";"))
+        return Arrays.stream(readDrinks.split(DRINKS_DELIMITER))
                 .map(Converter::substring)
                 .map(Converter::generateDrinkFrom)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), Drinks::new));
     }
 
     private static String substring(String readDrink) {
-        return readDrink.substring(1, readDrink.length() - 1);
+        return readDrink.substring(BEGIN_INDEX, readDrink.length() - TO_MAKE_END_INDEX_VALUE);
     }
 
     private static Drink generateDrinkFrom(String readDrink) {
-        String[] input = readDrink.split(",");
-        String name = input[0];
-        int price = Integer.parseInt(input[1]);
-        int quantity = Integer.parseInt(input[2]);
+        String[] input = readDrink.split(DRINK_DELIMITER);
+        String name = input[NAME_INDEX];
+        int price = Integer.parseInt(input[PRICE_INDEX]);
+        int quantity = Integer.parseInt(input[QUANTITY_INDEX]);
 
         return new Drink(name, price, quantity);
     }
